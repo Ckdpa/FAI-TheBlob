ff876b9f3e3bbfb255558ed07ef177f94d4fb95f
Ckdpa <felix.wirth@epita.com>
2023-10-06 20:27:56 +0200

    Creating initial GameBoard class files & CMake

---
+++ src/GameBoard.cpp
@@ -0,0 +1,5 @@
+//
+// Created by felix on 06/10/23.
+//
+
+#include "GameBoard.h"
---
+++ src/GameBoard.h
@@ -0,0 +1,14 @@
+//
+// Created by felix on 06/10/23.
+//
+
+#ifndef DOMINATORIX_GAMEBOARD_H
+#define DOMINATORIX_GAMEBOARD_H
+
+
+class GameBoard {
+
+};
+
+
+#endif //DOMINATORIX_GAMEBOARD_H
