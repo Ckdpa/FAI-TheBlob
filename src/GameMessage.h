a741d5cdbcc705f868dab6082b3d7d83d127ee88
Ckdpa <felix.wirth@epita.com>
2023-10-16 23:29:51 +0200

    Adding message type enumeration, preparing different message implementation

--- src/GameMessage.h
+++ src/GameMessage.h
@@ -6,9 +6,28 @@
 #define DOMINATORIX_GAMEMESSAGE_H
 
 
+#include <string>
+
 class GameMessage {
+public:
+    enum class MessageType {
+        SET,
+        HME,
+        MAP,
+        UPD,
+        END,
+        BYE,
+        UNKNOWN
+    };
+
+    GameMessage() = default;
+    virtual ~GameMessage() = default;
+    virtual void parse() = 0;
+    static MessageType parse_message_type(const std::string &bytes);
+};
+// Héritage ?
+class SetMessage : public GameMessage {
 
 };
-
 
 #endif //DOMINATORIX_GAMEMESSAGE_H
