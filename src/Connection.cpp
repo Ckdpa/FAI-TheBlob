e4b1046bbdcc915d13a8ea2d78116a9ea5f6e486
Ckdpa <felix.wirth@epita.com>
2023-10-16 15:12:10 +0200

    Adding initial game connection class

---
+++ src/Connection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by felix on 16/10/23.
+//
+
+#include "Connection.h"
---
+++ src/Connection.h
@@ -0,0 +1,14 @@
+//
+// Created by felix on 16/10/23.
+//
+
+#ifndef DOMINATORIX_CONNECTION_H
+#define DOMINATORIX_CONNECTION_H
+
+
+class Connection {
+
+};
+
+
+#endif //DOMINATORIX_CONNECTION_H
