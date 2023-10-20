b2a68e43d1491051e2b23e6b90b350864561b102
Ckdpa <felix.wirth@epita.com>
2023-10-20 15:15:18 +0200

    Adding implementation for HME Message

--- src/server/HMEMessage.cpp
+++ src/server/HMEMessage.cpp
@@ -5,5 +5,18 @@
 #include "HMEMessage.h"
 
 const char *HMEMessage::encode() const {
-    return nullptr;
+    return nullptr; // We should not send this message
+}
+
+HMEMessage::HMEMessage(const char *data)
+:row_(data[0]), col_(data[1]){
+
+}
+
+int HMEMessage::get_starting_row() const {
+    return row_;
+}
+
+int HMEMessage::get_starting_col() const {
+    return col_;
 }
--- src/server/HMEMessage.h
+++ src/server/HMEMessage.h
@@ -12,6 +12,13 @@ class HMEMessage : public GameMessage {
 public:
     const char* encode() const override;
 
+    explicit HMEMessage(const char* data);
+
+    int get_starting_row() const;
+    int get_starting_col() const;
+private:
+    int row_;
+    int col_;
 };
 
 
