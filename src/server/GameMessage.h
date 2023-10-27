96e0f43d33bbfbed2d1e39c42b3d83bf01bc294b
Ckdpa <felix.wirth@epita.com>
2023-10-27 18:33:10 +0200

    Adding message type enum to differentiate messages

--- src/server/GameMessage.h
+++ src/server/GameMessage.h
@@ -11,13 +11,25 @@
 class GameMessage {
 protected:
 public:
+    enum class MessageType {
+    END,
+    BYE,
+    HME,
+    HUM,
+    NME,
+    SET,
+    UPD,
+    MAP,
+};
     GameMessage() = default;
     virtual ~GameMessage() = default;
     /**
      * Translate the internal message in the byte representation to be sent to the server.
-     * @return
+     * @return the encoded message in string format to be written in socket
      */
     virtual std::string encode() const = 0;
+
+    virtual MessageType get_message_type() const = 0;
 };
 
 #endif //DOMINATORIX_GAMEMESSAGE_H
