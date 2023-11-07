fd85aa9ca7c3d9acf8613b10cf199ee1c2f5f7ec
Ckdpa <felix.wirth@epita.com>
2023-11-07 18:12:33 +0100

    MOVMessage minimal implementation

--- src/server/messages/MOVMessage.cpp
+++ src/server/messages/MOVMessage.cpp
@@ -2,16 +2,24 @@
 // Created by felix on 07/11/23.
 //
 
+#include <sstream>
 #include "MOVMessage.h"
 
-MOVMessage::MOVMessage(std::vector<Move> &moves) {
-
-}
+MOVMessage::MOVMessage(std::vector<Move> &moves)
+:moves_(moves)
+{}
 
 GameMessage::MessageType MOVMessage::get_message_type() const {
     return GameMessage::MessageType::MOV;
 }
 
 std::string MOVMessage::encode() const {
-    return std::string();
+    std::stringstream encoded_MOV("MOV");
+    encoded_MOV << static_cast<char>(moves_.size());
+    for (auto move : moves_) {
+        encoded_MOV << move.get_starting_x() << move.get_starting_y(); //  Source coordinates
+        encoded_MOV << move.get_number_entities();
+        encoded_MOV << move.get_ending_x() << move.get_ending_y(); // Target coordinates
+    }
+    return encoded_MOV.str();
 }
