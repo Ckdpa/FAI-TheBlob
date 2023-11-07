b090e83bec91b7ea61ff833804ab1530b5d6df5b
Ckdpa <felix.wirth@epita.com>
2023-11-07 18:04:55 +0100

    Adding Move implementation

--- src/Move.h
+++ src/Move.h
@@ -7,7 +7,20 @@
 
 
 class Move {
+public:
+    Move(char starting_row, char starting_col, char ending_row, char ending_col,  char number_entities);
 
+    char get_starting_x() const;
+    char get_starting_y() const;
+    char get_ending_x() const;
+    char get_ending_y() const;
+    char get_number_entities() const;
+private:
+    char starting_x_;
+    char starting_y_;
+    char ending_x_;
+    char ending_y_;
+    char number_entities_;
 };
 
 
