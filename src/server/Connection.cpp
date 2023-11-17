52656e0115c125cdabeae2e181625e13eb72c464
Félix <felix.wirth@epita.fr>
2023-11-17 15:36:43 +0100

    Updated Connection implementation from UNIX to Windows. Refactored and moved message reading and parsing to MessageHandler

--- src/server/Connection.cpp
+++ src/server/Connection.cpp
@@ -4,16 +4,25 @@
 
 #include "Connection.h"
 #include <stdexcept>
+/* Linux headers for socket, system
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <sys/poll.h>
+*/
+// Windows
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
 #include <stdexcept>
 #include <iostream>
 #include <cstring>
-#include <sys/poll.h>
 #include "server/MessageHandler.h"
 
+#define MAX_READING_TRIES 3
+
+/*
 Connection::Connection(const std::string& ip, int port) {
     ufds_[0].events = POLLIN;
     status_ = CONN_STATUS::DISCONNECTED;
@@ -96,4 +105,148 @@ GameMessage *Connection::read_socket() {
 //    read_bytes = read(conn_, &buff_ + total_bytes, 1024 - total_bytes);
     std::cout << "Message reading done" << std::endl;
     return MessageHandler::parse_message_type(std::string(buff_, ret)); // Will this greed through messages ?
+}
+*/
+
+Connection::Connection(const std::string& ip, int port) {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Failed to initialize Winsock\n";
+        throw std::runtime_error("Winsock initialization failed");
+    }
+
+    status_ = CONN_STATUS::DISCONNECTED;
+    conn_info_.sin_family = AF_INET;
+    
+    conn_info_.sin_port = htons(port);
+    conn_info_.sin_addr.s_addr = inet_addr(ip.c_str());
+//    inet_pton(AF_INET, ip.c_str(), &conn_info_.sin_addr);
+}
+
+Connection::~Connection() {
+    close_connection();
+    WSACleanup();
+}
+
+int Connection::close_connection() {
+    int status = closesocket(socket_);
+    status_ = CONN_STATUS::DISCONNECTED;
+    if (status == SOCKET_ERROR) {
+        std::cerr << "Error closing connection: " << WSAGetLastError() << "\n";
+    }
+    return status;
+}
+
+int Connection::connect_socket() {
+    socket_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_ == INVALID_SOCKET) {
+        std::cerr << "Could not open socket: " << WSAGetLastError() << "\n";
+        return -1;
+    }
+
+    if (connect(socket_, reinterpret_cast<const sockaddr*>(&conn_info_), sizeof(conn_info_)) == SOCKET_ERROR) {
+        std::cerr << "Error connecting to server: " << WSAGetLastError() << "\n";
+        closesocket(socket_);
+        return -1;
+    }
+
+    status_ = CONN_STATUS::CONNECTED;
+    return 0;
+}
+
+void Connection::socket_write(const GameMessage& message) {
+    std::string data = message.encode();
+    if (status_ != CONN_STATUS::CONNECTED) {
+        std::cerr << "Socket is not connected, cannot write message\n";
+        return;
+    }
+
+    size_t written_bytes = 0;
+    while (written_bytes < data.size()) {
+        auto write_status = send(socket_, data.c_str() + written_bytes, data.size() - written_bytes, 0);
+        if (write_status == SOCKET_ERROR) {
+            std::cerr << "Error while writing message: " << WSAGetLastError() << "\n";
+            status_ = CONN_STATUS::ERROR_STATE;
+            return;
+        }
+        written_bytes += write_status;
+    }
+}
+
+bool Connection::pending_message() {
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(socket_, &readfds);
+
+    // Use select for checking socket status
+    int result = select(0, &readfds, nullptr, nullptr, nullptr);
+    if (result == SOCKET_ERROR) {
+        std::cerr << "Error in select: " << WSAGetLastError() << "\n";
+        status_ = CONN_STATUS::ERROR_STATE;
+        return false;
+    }
+
+    return (result > 0) && FD_ISSET(socket_, &readfds);
+}
+
+int Connection::get_total_bytes_read() const {
+    return total_bytes_read_;
+}
+
+int Connection::get_total_bytes_used() const {
+    return total_bytes_used_;
+}
+
+void Connection::check_overflow(int n_chars) {
+    // Check for potential overflow
+    if (total_bytes_read_ + n_chars > sizeof(buff_)) {
+        // Flush the consumed data, not necessary anymore
+        memmove(buff_, buff_ + total_bytes_used_, total_bytes_read_ - total_bytes_used_);
+        // Update indexes
+        total_bytes_read_ =  total_bytes_read_ - total_bytes_used_;
+        total_bytes_used_ = 0;
+    }
+}
+
+bool Connection::read_socket(int n_chars) {
+    int read_bytes = 0;
+    int n_tries = 0;
+    while (read_bytes < n_chars){
+
+        // Check if we reached max number of tries
+        if (n_tries > MAX_READING_TRIES) {
+            std::cerr << "Reached " << MAX_READING_TRIES << " while trying to read n_chars: " << n_chars << std::endl;
+            return false;
+        }
+        // Check if we can read n_chars
+        check_overflow(n_chars);
+
+        // Try to fill the buffer
+        auto ret = recv(socket_, buff_ + total_bytes_read_, sizeof(buff_) - total_bytes_read_, 0);
+        if (ret == SOCKET_ERROR) {
+            std::cerr << "Socket error " << WSAGetLastError() << " while trying to read n_chars: " << n_chars << std::endl; 
+            return false;
+        }
+
+        // Update indexes
+        total_bytes_read_ += ret;
+        read_bytes += ret;
+        n_tries ++;
+        }
+    return true;
+}
+
+std::string Connection::consume_nchars(int n_chars) {
+    // Check if we do not have the data already
+    if (get_total_bytes_read() - get_total_bytes_used() < n_chars) {
+        // Read n_chars from socket.
+        if (!read_socket(n_chars)) {
+            return nullptr;
+        }
+    }
+    // Retrieve the data
+    std::string ret =  std::string(buff_ + total_bytes_used_, n_chars);
+    // Update the new used chars value
+    total_bytes_used_ += n_chars;
+    return ret;
 }
--- src/server/Connection.h
+++ src/server/Connection.h
@@ -6,20 +6,14 @@
 #define DOMINATORIX_CONNECTION_H
 
 #include <string>
-#include <netinet/in.h>
-#include <sys/poll.h>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
 #include "server/messages/GameMessage.h"
 
 // Connect to the server using TCP/IP Socket on a given port and IP adress
 class Connection {
-
+/*
 public:
-
-    enum class CONN_STATUS {
-        DISCONNECTED=0,
-        CONNECTED=1,
-        ERROR=2,
-    };
     Connection(const std::string& ip, int port);
     ~Connection();
     int connect_socket();
@@ -28,11 +22,36 @@ public:
     bool pending_message();
     GameMessage* read_socket();
 private:
+    int socket_init(void);
+    int socket_quit(void);
     struct sockaddr_in conn_info_{};
     CONN_STATUS status_;
     struct pollfd ufds_[1]{};
     char buff_[1024]{};
+*/
+public:
+
+    Connection(const std::string& ip, int port);
+    ~Connection();
+    int close_connection();
+    int connect_socket();
+    void socket_write(const GameMessage& message);
+    bool pending_message();
+    int get_total_bytes_read() const;
+    int get_total_bytes_used() const;
+    std::string consume_nchars(int n_chars);
+private:
+    bool read_socket(int n_chars);
+    void check_overflow(int n_chars);
+    int total_bytes_read_ = 0;
+    int total_bytes_used_ = 0;
+    SOCKET socket_{};
+    sockaddr_in conn_info_{};
+    enum class CONN_STATUS { DISCONNECTED, CONNECTED, ERROR_STATE };
+    CONN_STATUS status_;
+    char buff_[1024]{}; // Adjust the buffer size as needed
 };
+
 
 
 #endif //DOMINATORIX_CONNECTION_H
