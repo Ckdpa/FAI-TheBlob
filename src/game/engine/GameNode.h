db1e4cae99d17437e246d8879ddf232ff57f8c1e
Félix <felix.wirth@epita.fr>
2023-12-19 08:23:14 +0100

    Testing alpha betas

--- src/game/engine/GameNode.h
+++ src/game/engine/GameNode.h
@@ -15,19 +15,29 @@ class GameNode {
 public:
 // Alpha beta
     explicit GameNode(Game game);
+    // Wikipedia
+    std::vector<Move> get_next_move(int depth=5);
 
-    std::vector<Move> get_next_move(int depth=6);
-    std::vector<Move> get_next_move_2(int depth=7);
+    // Chess Programming
+    std::vector<Move> get_next_move_2(int depth=5);
+
+    // Negamax
+    std::vector<Move> get_next_move_3(int depth=5);
 private:
     // If we are at root, this pointer leads to the associated board.
     std::unique_ptr<Game> game_;
     std::vector<std::unique_ptr<GameNode>> reachable_games_;
     std::vector<std::vector<Move>> order_moves(const std::vector<std::vector<Move>> &vector);
 
-    double alpha_beta(int depth, double alpha, double beta, bool maximizing);
+    // Wikipedia
+    int alpha_beta(int depth, int alpha, int beta, bool maximizing);
 
+    // Chess Programming
     int alpha_beta_max(int alpha, int beta, int depthleft);
     int alpha_beta_min(int alpha, int beta, int depthleft);
+
+    // Safe
+    int negamax(int depth);
 };
 
 
